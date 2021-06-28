5791400ae0d0793b521580fc2aaf89e58bf847c0
narotam-0909 <narotambalotra@gmail.com>
2021-06-28 13:38:27 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+              <img src="https://media.giphy.com/media/3jVT4U5bilspG/giphy.gif" alt="logo" />
+              <h2> I love samosa</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
