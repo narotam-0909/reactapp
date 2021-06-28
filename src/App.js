b9575833a816038ba887b13ea31308637ef64005
narotam-0909 <narotambalotra@gmail.com>
2021-06-28 16:01:47 +0800

    my 2nd commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
               <img src="https://media.giphy.com/media/3jVT4U5bilspG/giphy.gif" alt="logo" />
-              <h2> I love samosa</h2>
+              <h2> I love Coffie </h2>
       </header>
     </div>
   );
